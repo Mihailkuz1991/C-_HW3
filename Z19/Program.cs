b87c81cb85ecd933d4de341f1e9b03e2965a5f28
Michael Kuznetsov <mihasyuga@yandex.ru>
2022-12-28 11:51:13 +0300

    C#_HW3

---
+++ Z19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+Console.Clear();
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+else 
+{
+    int Digit1 = num / 10000;
+    int Digit2 = num / 1000 % 10;
+    int Digit3 = num / 100 % 10;
+    int Digit4 = num / 10 % 10;
+    int Digit5 = num % 10;
+
+    
+    if (Digit1 == Digit5 && Digit2 == Digit4)
+    Console.WriteLine("Число является полиндромом");
+    else Console.WriteLine("Число не является полиндромом");
+};
---
+++ Z21/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит 
+расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Clear();
+
+int getUserValue(string massage)
+{
+    Console.WriteLine(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int xa1 = getUserValue("Введите кординату X числа A");
+int ya1 = getUserValue("Введите кординату Y числа A");
+int za1 = getUserValue("Введите кординату Z числа A");
+
+int xb2 = getUserValue("Введите кординату X числа B");
+int yb2 = getUserValue("Введите кординату Y числа B");
+int zb2 = getUserValue("Введите кординату Z числа B");
+
+double GetDist(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double dx = xa - xb;
+    double dy = ya - yb;
+    double dz = za - zb;
+
+    return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+double result = GetDist(xa1, ya1, za1, xb2, yb2, zb2);
+Console.WriteLine($"Расстояние между точками A ({xa1}, {ya1}, {za1}) и B ({xb2}, {yb2}, {zb2}) равно {Math.Round(result, 2)}");
